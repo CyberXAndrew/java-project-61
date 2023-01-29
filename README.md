8a0ae4a77642086554ecc321edf0bb3dd837468f
Andrey Borisov <royalxxx@yandex.ru>
2023-01-29 12:04:38 +0300

    fix README.md build.gradle App.java

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/CyberXAndrew/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/CyberXAndrew/java-project-61/actions)
+<<<<<<< Updated upstream
 https://asciinema.org/a/8aDNch76uzHN8Rvx5Gbf7CHmN (game Even - win);
 https://asciinema.org/a/DqLZRXslgcUdTybBU90iyBCVH (game Even - lose);
 https://asciinema.org/a/ihY8p3OsLJRDtlWytqV0GjxUP (game Calc - win);
 https://asciinema.org/a/zQKKKM8w7tOAS2bVDAzcCTpXx (game Calc - win);
+=======
+[![Maintainability](https://api.codeclimate.com/v1/badges/6c17a32b8e219dd473e8/maintainability)](https://codeclimate.com/github/CyberXAndrew/java-project-61/maintainability)
+>>>>>>> Stashed changes
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,37 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        String userChoise = scanner.next();
+        if (userChoise.equals("1")) {
+            System.out.println("Your choise: 1");
+            System.out.println();
+            System.out.println("Welcome to the Brain Games!");
+            Cli.ask();
+        } else if (userChoise.equals("2")) {
+            System.out.println("Your choise: 2");
+            System.out.println();
+            System.out.println("Welcome to the Brain Games!");
+            Even.isParity();
+        } else if (userChoise.equals("3")) {
+            System.out.println("Your choise: 3");
+            System.out.println();
+            System.out.println("Welcome to the Brain Games!");
+            Calc.calculator();
+        } else if (userChoise.equals("0")) {
+            System.out.println("Your choise: 0");
+            scanner.close();
+        }
     }
 }
